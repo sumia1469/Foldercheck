73abe4ecc6888bcaec6cc8fa4301b7b26093e404
sumia1469 <sumia@naver.com>
2025-04-01 23:57:39 +0000

    파일추가

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const fs=require('fs')
+const path=require('path')
+const {exec} = require('child_process')
+
+const PORT=4400
+const interval=3000
+let fileTimestamps={}
+let watchedFolders=[]
+let networkCredentials={}
+
+function connectToNetwork(username,password,serverIP){
+    try{
+      console
+    }catch(e){
+
+    }
+}

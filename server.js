05ee739f254b0deeb47077ea7b1450bd0527e325
sumia1469 <sumia@naver.com>
2025-04-02 00:04:48 +0000

    serv기개선

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const fs=require('fs')
 const path=require('path')
-const {exec} = require('child_process')
+const {exec, execSync} = require('child_process')
 
 const PORT=4400
 const interval=3000
@@ -11,7 +11,16 @@ let networkCredentials={}
 
 function connectToNetwork(username,password,serverIP){
     try{
-      console
+      console.log(`네트워크 폴더 로그인: &{serverIP}`);
+      execSync(`use net \\\\${serverIP} /user:${username} ${password}`, {stdio:'ignore'});
+    }catch(e){
+console.error(`네트워크 로그인 실패:${e.message}`);
+    }
+}
+
+function loadwatchedFolders(){
+    try{
+const data=fs.readFileSync('folderList.json', 'utf8');
     }catch(e){
 
     }
